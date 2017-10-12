510b14a11a6591af3e5762a77f4da5085d507750
Daniel Toniuc <dtoniuc@gmail.com>
2017-10-12 14:49:33 +0300

    Assignment finished
    Roman to arabic conversion using TDD methodology.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,118 @@
-
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+	
+	private String romanNumerals;
+	private String romanFiveNumerals;
+	private int[] romanNumeralLevels;
+	private int[] romanNumeralValues;
+	
+	public RomanNumerals() {
+		romanNumerals = "MDCLXVI";
+		romanFiveNumerals = "DLV";
+		romanNumeralLevels = new int [] {4, 4, 3, 3, 2, 2, 1};
+		romanNumeralValues = new int [] {1000, 500, 100, 50, 10, 5, 1};
+	}
+	
+	public int convertToInteger(String romanNum) throws RomanNumeralsException  {
 		
+		if(!isValidRomanNumerals(romanNum)) {
+			throw new RomanNumeralsException();
+		}
+
+		return computeValue(romanNum);
+	}
+	
+	private boolean isValidRomanNumerals(String romanNum) {
+		if(romanNum.isEmpty()) return false;
+		if(hasInvalidRomanCharacters(romanNum)) return false;
+		if(hasTooManySeqCharacters(romanNum)) return false;
+		if(hasInvalidSubtracations(romanNum)) return false;
+		
+		return true;
+	}
+	
+	private boolean hasInvalidRomanCharacters(String str) {
+		for(int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			
+			if(romanNumerals.indexOf(ch) == -1)
+				return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean hasTooManySeqCharacters(String str) {
+		char prev = str.charAt(0); 
+		int count = 1;
+		
+		for(int i = 1; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			
+			if(ch != prev) {
+				count = 0;
+			}
+			
+			if((count == 3) || (count == 1 && isFiveNumeral(ch))) {
+				return true;
+			}
+			
+			prev = ch;
+			count++;
+		}
+		
+		return false;
+	}
+	
+	private boolean hasInvalidSubtracations(String str) {
+		char[] charList = reverseString(str).toCharArray();
+		
+		int twoPrevLevel = 0;
+		int onePrevLevel = 0;
+		
+		for(char x : charList) {
+			int alphabetIndex = romanNumerals.indexOf(x);
+			int level = romanNumeralLevels[alphabetIndex];
+			
+			if(level < onePrevLevel - 1) return true;
+			if(level <= onePrevLevel && isFiveNumeral(x)) return true;
+			if(level == onePrevLevel && level == (twoPrevLevel - 1)) return true;
+			if(level == (onePrevLevel - 1) && twoPrevLevel == (onePrevLevel - 1)) return true;
+			
+			twoPrevLevel = onePrevLevel;
+			onePrevLevel = level;
+		}
+		
+		return false;
+	}
+	
+	private boolean isFiveNumeral(char ch) {
+		return (romanFiveNumerals.indexOf(ch) != -1);
+	}
+	
+	private int computeValue(String romanNum) {
+		String tmp = reverseString(romanNum);
+		int sum = 0;
+		int prevValue = 0;
+		
+		for(int i = 0; i < tmp.length(); i++) {
+			char ch = tmp.charAt(i);
+			int index = romanNumerals.indexOf(ch);
+			int value = romanNumeralValues[index];
+		
+			if(value < prevValue) {
+				sum -= value;
+			}
+			else {
+				sum += value;
+			}
+			
+			prevValue = value;
+		}
+		
+		return sum;
+	}
+	
+	private String reverseString(String str) {
+		return new StringBuilder(str).reverse().toString();
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,360 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void convertToInteger_validNumeral_notThrowsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String validNumeral = "MMCMXXXIV";
+		
+		//Act
+		int actual = converter.convertToInteger(validNumeral);
+		
+		//Assert
+		int expected = 2934;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void convertToInteger_validNumera2_notThrowsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String validNumeral = "CDXLIV";
+		
+		//Act
+		int actual = converter.convertToInteger(validNumeral);
+		
+		//Assert
+		int expected = 444;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void convertToInteger_validNumera3_notThrowsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String validNumeral = "MMMCCCXXXIII";
+		
+		//Act
+		int actual = converter.convertToInteger(validNumeral);
+		
+		//Assert
+		int expected = 3333;
+		assertEquals(expected, actual);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralEmptyString_notThrowsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	//More than one '5' characters
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralTwo_D_Chars_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "DD";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralTwo_L_Chars_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "LL";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralTwo_V_Chars_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "VV";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
 	}
 
+	//More than three '1' characters
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralFour_M_Chars_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "MMMM";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralFour_C_Chars_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "CCCC";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralFour_X_Chars_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "XXXX";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralFour_I_Chars_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "IIII";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	//Double subtraction
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralDouble_CCM_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "CCM";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralDouble_CCD_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "CCD";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralDouble_XXC_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "XXC";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralDouble_XXL_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "XXL";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralDouble_IIX_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "IIX";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralDouble_IIV_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "IIV";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+
+	//Invalid '5' character subtraction
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_DM_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "DM";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_LM_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "LM";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_VM_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "VM";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_LC_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "LC";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_VC_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "VC";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_VX_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "VX";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	//Invalid '1' character subtraction
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_XM_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "XM";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_IM_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "IM";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_XD_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "XD";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_IC_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "IC";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBad_IL_Subtraction_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "IL";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	//Invalid character sequence
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBAD_CMC_Sequence_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "CMC";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBAD_CDC_Sequence_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "CMC";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBAD_XCX_Sequence_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "XCX";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBAD_XLX_Sequence_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "XLX";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBAD_IXI_Sequence_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "IXI";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
+	
+	@Test(expected = RomanNumeralsException.class)
+	public void convertToInteger_invalidNumeralBAD_IVI_Sequence_throwsException() throws RomanNumeralsException {
+		//Arrange
+		RomanNumerals converter = new RomanNumerals();
+		String invalidNumeral = "IVI";
+		
+		//Act
+		converter.convertToInteger(invalidNumeral);
+	}
 }
